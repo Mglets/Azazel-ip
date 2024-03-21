f7dd5278ca4c8b24d0ac16d458c7a25c970f4ba8
Mglets <164342152+Mglets@users.noreply.github.com>
2024-03-21 21:59:37 +0200

    Update Azazel.py

--- Azazel.py
+++ Azazel.py
@@ -1,24 +1,22 @@
 
 import requests
 
-def get_ip_info(ip_address):
-    url = f"http://ip-api.com/json/{ip_address}"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        return None
+print("مرحبًا بك في الأداة! قم بإدخال عنوان IP للحصول على المعلومات.")
 
-# مثال على استخدام الدالة
-ip_address = 
-ip_info = get_ip_info(ip_address)
-if ip_info:
-    print(f"معلومات عنوان IP {ip_address}:")
-    print(f"الدولة: {ip_info[ country ]}")
-    print(f"المنطقة: {ip_info[ regionName ]}")
-    print(f"المدينة: {ip_info[ city ]}")
-    # ومعلومات أخرى يمكن الوصول إليها
-else:
-    print(f"تعذر الحصول على معلومات عنوان IP {ip_address}")
+ip = input("عنوان IP: ")
+
+try:
+    response = requests.get(f"http://ip-api.com/json/{ip}")
+    data = response.json()
+
+    # استخراج المعلومات التي ترغب في عرضها من الاستجابة
+    country = data["country"]
+    city = data["city"]
+    isp = data["isp"]
+
+    # عرض المعلومات
+    print(f"الدولة: {country}")
+    print(f"المدينة: {city}")
+    print(f"مزود الخدمة: {isp}")
+except:
+    print("حدث خطأ أثناء إرسال عنوان IP. يرجى التأكد من صحة عنوان IP وإعادة المحاولة.")
